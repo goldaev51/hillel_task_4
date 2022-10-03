423c20ba436f45b78add9a71a0e3289edd0d5e79
Artem <38584948+goldaev51@users.noreply.github.com>
2022-10-04 01:16:59 +0300

    increase code

--- task_4.py
+++ task_4.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 def main_page():
     text = '<p>Welcome to Task_4 created by Artem Goldaev</p>'
     text += f'''
-    <a href={url_for("requirements")}>Requirements page</a></br>
-    <a href={url_for("generate_users")}>Generate-users page</a></br>
-    <a href={url_for("mean")}>Mean page</a></br>
-    <a href={url_for("space")}>Space page</a></br>
+    <a href={url_for("requirements")}>Requirements page</a><br>
+    <a href={url_for("generate_users")}>Generate-users page</a><br>
+    <a href={url_for("mean")}>Mean page</a><br>
+    <a href={url_for("space")}>Space page</a><br>
     '''
     return text
 
@@ -28,7 +28,7 @@ def requirements():
         lib_formatted = lib.replace('\n', '')
         res += f'<p>{lib_formatted}</p>'
 
-    return f'<a href={url_for("main_page")}>Main page</a></br>' + res
+    return f'<a href={url_for("main_page")}>Main page</a><br>' + res
 
 
 @app.route('/generate-users/', methods=['GET'])
@@ -37,19 +37,17 @@ def generate_users():
     args = request.args
     count = args.get('count')
     if count is None:
-        return f'<a href={url_for("main_page")}>Main page</a></br>' \
+        return f'<a href={url_for("main_page")}>Main page</a><br>' \
                f'<p>You should add parameter count to see results</p>'
-    try:
-        count = int(count)
-    except ValueError as e:
-        return f'<a href={url_for("main_page")}>Main page</a></br>' \
+    if not count.isdigit():
+        return f'<a href={url_for("main_page")}>Main page</a><br>' \
                f'<p>Parameter count must have int</p>'
-    users = f'<a href={url_for("main_page")}>Main page</a></br>'
+    count = int(count)
+
     users_emails = []
     for _ in range(count):
         first_name = fake.first_name()
         user_email = (first_name + fake.last_name()).lower() + '@' + fake.free_email_domain()
-        users += f'<p>{first_name} {user_email}</p>'
         users_emails.append({'name': first_name, 'email': user_email})
 
     return render_template('emails.html', emails=users_emails)
@@ -61,18 +59,18 @@ def mean():
     mean_height = round(df['Height(Inches)'].mean() * 2.54, 2)
     mean_weight = round(df['Weight(Pounds)'].mean() * 0.453592, 2)
     return f'''
-    <a href={url_for("main_page")}>Main page</a></br>
-    <p>mean_height = {mean_height}</p>
-    <p>mean_weight = {mean_weight}</p>
+    <a href={url_for("main_page")}>Main page</a><br>
+    <p>mean_height = {mean_height} cm</p>
+    <p>mean_weight = {mean_weight} kg</p>
     '''
 
 
 @app.route('/space/')
 def space():
     r = requests.get('http://api.open-notify.org/astros.json')
-    astronaut = r.json()['number']
-    return f'<a href={url_for("main_page")}>Main page</a></br>' \
-           f'<p>Number of astronauts in space: {astronaut}</p>'
+    astronauts = sorted(r.json()['people'], key=lambda d: d['craft'])
+    astronaut_count = r.json()['number']
+    return render_template('space.html', astronaut_count=astronaut_count, astronauts=astronauts)
 
 
 if __name__ == '__main__':
